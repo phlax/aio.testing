0a0502319fd14d80042d2b2f1ef59bd5e1b85ae0
Ryan Northey <ryan@space23.net>
2015-05-07 17:18:02 +0100

    adding setuptools boilerplate

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+aio.testing
+"""
+
+from setuptools import setup, find_packages
+
+from aio.testing import __version__ as version
+
+
+setup(
+    name='aio.testing',
+    version=version,
+    description="Aio testing utils",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    keywords='',
+    author='Ryan Northey',
+    author_email='ryan@3ca.org.uk',
+    url='http://github.com/phlax/aio.testing',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['aio', 'aio.testing'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """)
